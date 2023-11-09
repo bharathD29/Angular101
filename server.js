b4b3a206191def3a84bdd99e1788e7a369ad0496
Bharath Dasari <dasari.bharath14@yahoo.com>
2023-11-10 03:33:00 +0530

    initail check

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular101'));
+app.use(express.static(__dirname + '/dist/angular101/browser'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/angular101/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angular101/browser/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
